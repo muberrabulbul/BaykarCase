ad745d4585c99ca987ca825186aa251c0526f420
muberrabulbull <muberrabulbul97@gmail.com>
2024-03-24 02:06:17 +0300

    added docker configuration

--- baykarcase/settings.py
+++ baykarcase/settings.py
@@ -106,14 +106,15 @@ WSGI_APPLICATION = "baykarcase.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "bdb",
-        "USER": "postgres",
-        "PASSWORD": "mypassword.",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "ENGINE": os.environ.get("DB_DRIVER", "django.db.backends.postgresql"),
+        "NAME": os.environ.get("PG_DB", "postgres"),
+        "USER": os.environ.get("PG_USER", "postgres"),
+        "PASSWORD": os.environ.get("PG_PASSWORD", "postgres"),
+        "HOST": os.environ.get("PG_HOST", "localhost"),
+        "PORT": os.environ.get("PG_PORT", "5432"),
     }
 }
 
